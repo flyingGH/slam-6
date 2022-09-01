c7bfc9c9d26d2ca0c0c5c8efd9e63b196cfb371e
applejxd <9639592+applejxd@users.noreply.github.com>
2022-09-02 08:16:03 +0900

    add comments

--- modules/mono-vo/test.py
+++ modules/mono-vo/test.py
@@ -12,6 +12,7 @@ def main():
     cam = PinholeCamera(width=1241.0, height=376.0, fx=718.8560, fy=718.8560, cx=607.1928, cy=185.2157)
     vo = VisualOdometry(cam, f'{root_path}/data_odometry_poses/dataset/poses/00.txt')
 
+    # image for trajectory drawing
     traj = np.zeros((600, 600, 3), dtype=np.uint8)
 
     for img_id in range(4541):
@@ -21,17 +22,28 @@ def main():
 
         cur_t = vo.cur_t
         if img_id > 2:
+            cur_t = cur_t.reshape(-1)
             x, y, z = cur_t[0], cur_t[1], cur_t[2]
         else:
             x, y, z = 0., 0., 0.
-        draw_x, draw_y = int(x) + 290, int(z) + 90
-        true_x, true_y = int(vo.trueX) + 290, int(vo.trueZ) + 90
 
-        cv2.circle(traj, (draw_x, draw_y), 1, (img_id * 255 / 4540, 255 - img_id * 255 / 4540, 0), 1)
-        cv2.circle(traj, (true_x, true_y), 1, (0, 0, 255), 2)
-        cv2.rectangle(traj, (10, 20), (600, 60), (0, 0, 0), -1)
-        text = "Coordinates: x=%2fm y=%2fm z=%2fm" % (x, y, z)
-        cv2.putText(traj, text, (20, 40), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1, 8)
+        # convert to the image coordinate
+        # (swap variables and add offsets)
+        draw_x, draw_y = int(z) + 90, int(x) + 290
+        true_x, true_y = int(vo.trueZ) + 90, int(vo.trueX) + 290
+
+        # estimated trajectory (smaller line, green -> blue)
+        cv2.circle(traj, (draw_x, draw_y), radius=1,
+                   color=(img_id * 255 / 4540, 255 - img_id * 255 / 4540, 0), thickness=1)
+        # true trajectory (larger line & red)
+        cv2.circle(traj, (true_x, true_y), radius=1, color=(0, 0, 255), thickness=2)
+        # background color (black)
+        cv2.rectangle(traj, pt1=(10, 20), pt2=(600, 60), color=(0, 0, 0), thickness=-1)
+
+        # put text (white) from upper left side of the trajectory window
+        text = f"Coordinates: x={x:2f}m y={y:2f}m z={z:2f}m"
+        cv2.putText(traj, text, org=(20, 40), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=1,
+                    color=(255, 255, 255), thickness=1, lineType=8)
 
         cv2.imshow('Road facing camera', img)
         cv2.imshow('Trajectory', traj)
--- modules/mono-vo/visual_odometry.py
+++ modules/mono-vo/visual_odometry.py
@@ -3,22 +3,39 @@ from https://github.com/uoip/monoVO-python
 """
 import numpy as np
 import cv2
+from typing import Tuple
 
+# image processing mode
 STAGE_FIRST_FRAME = 0
 STAGE_SECOND_FRAME = 1
 STAGE_DEFAULT_FRAME = 2
+
+# minimum keypoint number to refresh
 kMinNumFeature = 1500
 
+# parameters for optical flow
 lk_params = dict(winSize=(21, 21),
                  # maxLevel = 3,
                  criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 30, 0.01))
 
 
 def feature_tracking(image_ref, image_cur, px_ref):
-    kp2, st, err = cv2.calcOpticalFlowPyrLK(image_ref, image_cur, px_ref, None, **lk_params)  # shape: [k,2] [k,1] [k,1]
+    """
+    calc optical flow
 
+    :param image_ref: previous image mat
+    :param image_cur: current image mat
+    :param px_ref: feature points of previous image
+    :return: start points and end points of optical flow
+    """
+    # shape: [k,2] [k,1] [k,1]
+    kp2, st, err = cv2.calcOpticalFlowPyrLK(image_ref, image_cur, prevPts=px_ref, nextPts=None, **lk_params)
+
+    # bool list for optical flow (same length with px_ref)
     st = st.reshape(st.shape[0])
+    # extract feature points as start points of optical flows
     kp1 = px_ref[st == 1]
+    # extract feature points as end points of optical flows
     kp2 = kp2[st == 1]
 
     return kp1, kp2
@@ -34,7 +51,7 @@ class PinholeCamera:
         self.cx = cx
         self.cy = cy
         self.distortion = (abs(k1) > 0.0000001)
-        self.d = [k1, k2, p1, p2, k3]
+        self.d = np.array([k1, k2, p1, p2, k3])
 
     def get_camera_mat(self):
         return np.array([[self.fx, 0, self.cx],
@@ -43,7 +60,14 @@ class PinholeCamera:
 
 
 class VisualOdometry:
+    """
+    visual odometry by sparce direct method (use feature points & optical flow)
+    """
     def __init__(self, cam, annotations):
+        """
+        :param cam: inner camera model parameters
+        :param annotations: path to annotation data for getting scale info
+        """
         self.frame_stage = 0
         self.cam: PinholeCamera = cam
         self.new_frame = None
@@ -55,58 +79,106 @@ class VisualOdometry:
         self.focal = cam.fx
         self.pp = (cam.cx, cam.cy)
         self.trueX, self.trueY, self.trueZ = 0, 0, 0
+
         self.detector = cv2.FastFeatureDetector_create(threshold=25, nonmaxSuppression=True)
+
         with open(annotations) as f:
             self.annotations = f.readlines()
 
-    def get_absolute_scale(self, frame_id):  # specialized for KITTI odometry dataset
+    def get_absolute_scale(self, frame_id):
+        """
+        get scale data (specialized for KITTI odometry dataset)
+
+        :param frame_id:
+        :return:
+        """
         ss = self.annotations[frame_id - 1].strip().split()
         x_prev = float(ss[3])
         y_prev = float(ss[7])
         z_prev = float(ss[11])
+
         ss = self.annotations[frame_id].strip().split()
         x = float(ss[3])
         y = float(ss[7])
         z = float(ss[11])
+
         self.trueX, self.trueY, self.trueZ = x, y, z
+
+        # 3D Pythagorean theorem
         return np.sqrt((x - x_prev) * (x - x_prev) + (y - y_prev) * (y - y_prev) + (z - z_prev) * (z - z_prev))
 
     def process_first_frame(self):
-        self.px_ref = self.detector.detect(self.new_frame)
+        """
+        only detect feature points
+        """
+        self.px_ref: Tuple[cv2.KeyPoint] = self.detector.detect(self.new_frame)
         self.px_ref = np.array([x.pt for x in self.px_ref], dtype=np.float32)
+        # move to the next processing stage
         self.frame_stage = STAGE_SECOND_FRAME
 
     def process_second_frame(self):
+        """
+        get optical flow and apply 5-point algorithm
+        """
+        # get optical flow
         self.px_ref, self.px_cur = feature_tracking(self.last_frame, self.new_frame, self.px_ref)
+
         # e_mat, mask = cv2.findEssentialMat(self.px_cur, self.px_ref, focal=self.focal, pp=self.pp, method=cv2.RANSAC,
         #                                    prob=0.999, threshold=1.0)
         e_mat, mask = cv2.findEssentialMat(
             self.px_cur, self.px_ref, cameraMatrix=self.cam.get_camera_mat(), method=cv2.RANSAC,
             prob=0.999, threshold=1.0)
-        _, self.cur_R, self.cur_t, mask = cv2.recoverPose(e_mat, self.px_cur, self.px_ref, focal=self.focal, pp=self.pp)
-        self.frame_stage = STAGE_DEFAULT_FRAME
+
+        # 5-point algorithm
+        _, self.cur_R, self.cur_t, mask = cv2.recoverPose(
+            e_mat, self.px_cur, self.px_ref, focal=self.focal, pp=self.pp)
+
         self.px_ref = self.px_cur
+        # move to the next processing stage
+        self.frame_stage = STAGE_DEFAULT_FRAME
 
     def process_frame(self, frame_id):
+        """
+        use 5-point algorithm and apply homogeneous transformation
+
+        :param frame_id:
+        :return:
+        """
         self.px_ref, self.px_cur = feature_tracking(self.last_frame, self.new_frame, self.px_ref)
         # e_mat, mask = cv2.findEssentialMat(self.px_cur, self.px_ref, focal=self.focal, pp=self.pp, method=cv2.RANSAC,
         #                                    prob=0.999, threshold=1.0)
         e_mat, mask = cv2.findEssentialMat(
             self.px_cur, self.px_ref, cameraMatrix=self.cam.get_camera_mat(), method=cv2.RANSAC,
             prob=0.999, threshold=1.0)
-        _, rot_mat, t, mask = cv2.recoverPose(e_mat, self.px_cur, self.px_ref, focal=self.focal, pp=self.pp)
+        # 5-point algorithm
+        _, rot_mat, t, mask = cv2.recoverPose(
+            e_mat, self.px_cur, self.px_ref, focal=self.focal, pp=self.pp)
+
+        # homogeneous transformation
         absolute_scale = self.get_absolute_scale(frame_id)
         if absolute_scale > 0.1:
             self.cur_t = self.cur_t + absolute_scale * self.cur_R.dot(t)
             self.cur_R = rot_mat.dot(self.cur_R)
+
+        # get additional feature points if remaining are not enough
         if self.px_ref.shape[0] < kMinNumFeature:
             self.px_cur = self.detector.detect(self.new_frame)
             self.px_cur = np.array([x.pt for x in self.px_cur], dtype=np.float32)
+
         self.px_ref = self.px_cur
 
     def update(self, img, frame_id):
-        assert (img.ndim == 2 and img.shape[0] == self.cam.height and img.shape[1] == self.cam.width), \
+        """
+        update position and pose
+
+        :param img: current video image
+        :param frame_id:
+        """
+        is_gray = img.ndim == 2
+        is_same_size = img.shape[0] == self.cam.height and img.shape[1] == self.cam.width
+        assert (is_gray and is_same_size), \
             "Frame: provided image has not the same size as the camera model or image is not grayscale"
+
         self.new_frame = img
         if self.frame_stage == STAGE_DEFAULT_FRAME:
             self.process_frame(frame_id)
