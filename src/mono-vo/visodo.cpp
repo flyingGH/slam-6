6b65545061c6fe7508e3de95aef72a528f7291bd
applejxd <9639592+applejxd@users.noreply.github.com>
2022-09-02 20:36:44 +0900

    refactored

--- src/mono-vo/visodo.cpp
+++ src/mono-vo/visodo.cpp
@@ -29,6 +29,11 @@ const int MAX_FRAME = 1000;
 const int MIN_NUM_FEAT = 2000;
 const string root_path = "/mnt/d/datasets/KITTI/odometry";
 
+// TODO: add a function to load these values directly from KITTI's calib files
+// WARNING: different sequences in the KITTI VO dataset have different intrinsic/extrinsic parameters
+const double focal = 718.8560;
+const cv::Point2d pp(607.1928, 185.2157);
+
 // IMP: Change the file directories (4 places) according to where your dataset is saved before running!
 
 double getAbsoluteScale(int frame_id) {
@@ -64,48 +69,62 @@ double getAbsoluteScale(int frame_id) {
     return sqrt((x - x_prev) * (x - x_prev) + (y - y_prev) * (y - y_prev) + (z - z_prev) * (z - z_prev));
 }
 
+void initialize_images(Mat &prevImage, Mat &currImage) {
+    const string filename1(root_path + "/data_odometry_color/dataset/sequences/00/image_2/000000.png");
+    const string filename2(root_path + "/data_odometry_color/dataset/sequences/00/image_2/000001.png");
 
-int main(int argc, char **argv) {
-    double scale = 1.00;
+    //read the first two frames from the dataset
+    const Mat img_1_c = imread(filename1);
+    const Mat img_2_c = imread(filename2);
 
-    Mat img_1, img_2;
-    {
-        const string filename1(root_path + "/data_odometry_color/dataset/sequences/00/image_2/000000.png");
-        const string filename2(root_path + "/data_odometry_color/dataset/sequences/00/image_2/000001.png");
-
-        //read the first two frames from the dataset
-        const Mat img_1_c = imread(filename1);
-        const Mat img_2_c = imread(filename2);
-
-        if (!img_1_c.data || !img_2_c.data) {
-            std::cout << " --(!) Error reading images " << std::endl;
-            return -1;
-        }
-
-        // we work with grayscale images
-        cvtColor(img_1_c, img_1, COLOR_BGR2GRAY);
-        cvtColor(img_2_c, img_2, COLOR_BGR2GRAY);
+    if (!img_1_c.data || !img_2_c.data) {
+        throw runtime_error("Error reading images");
     }
 
-    // feature detection, tracking
-    vector<Point2f> points1, points2;        //vectors to store the coordinates of the feature points
-    featureDetection(img_1, points1);        //detect features in img_1
-    vector<uchar> status;
-    featureTracking(img_1, img_2, points1, points2, status); //track those features to img_2
+    // we work with grayscale images
+    cvtColor(img_1_c, prevImage, COLOR_BGR2GRAY);
+    cvtColor(img_2_c, currImage, COLOR_BGR2GRAY);
+}
 
-    // TODO: add a function to load these values directly from KITTI's calib files
-    // WARNING: different sequences in the KITTI VO dataset have different intrinsic/extrinsic parameters
-    const double focal = 718.8560;
-    const cv::Point2d pp(607.1928, 185.2157);
+void write_trajectory(const Mat &t_f, Mat &traj) {
+
+    const int x = int(t_f.at<double>(0)) + 300;
+    const int y = int(t_f.at<double>(2)) + 100;
+    circle(traj, Point(x, y), 1, CV_RGB(255, 0, 0), 2);
+
+
+    rectangle(traj, Point(10, 30), Point(550, 50), CV_RGB(0, 0, 0), FILLED);
+
+    const string text = (boost::format("Coordinates: x = %02fm y = %02fm z = %02fm")
+                         % t_f.at<double>(0) % t_f.at<double>(1) % t_f.at<double>(2)).str();
+    const int fontFace = FONT_HERSHEY_PLAIN;
+    const double fontScale = 1;
+    const int thickness = 1;
+    const Point textOrg(10, 50);
+    putText(traj, text, textOrg, fontFace, fontScale, Scalar::all(255), thickness, 8);
+
+}
+
+int main(int argc, char **argv) {
+    ofstream myfile;
+    myfile.open("results1_1.txt");
+
+    Mat prevImage, currImage;
+    initialize_images(prevImage, currImage);
+
+    // feature detection, tracking
+    vector<Point2f> prevFeatures, currFeatures;        //vectors to store the coordinates of the feature points
+    featureDetection(prevImage, prevFeatures);        //detect features in img_1
+    vector<uchar> status;
+    featureTracking(prevImage, currImage, prevFeatures, currFeatures, status); //track those features to img_2
+
     //recovering the pose and the essential matrix
     Mat E, R, t, mask;
-    E = findEssentialMat(points2, points1, focal, pp, RANSAC, 0.999, 1.0, mask);
-    recoverPose(E, points2, points1, R, t, focal, pp, mask);
+    E = findEssentialMat(currFeatures, prevFeatures, focal, pp, RANSAC, 0.999, 1.0, mask);
+    recoverPose(E, currFeatures, prevFeatures, R, t, focal, pp, mask);
 
-    Mat prevImage = img_2;
-    Mat currImage;
-    vector<Point2f> prevFeatures = points2;
-    vector<Point2f> currFeatures;
+    prevImage = currImage;
+    prevFeatures = currFeatures;
 
     Mat R_f(R.clone());
     Mat t_f(t.clone());
@@ -116,12 +135,11 @@ int main(int argc, char **argv) {
     namedWindow("Trajectory", WINDOW_AUTOSIZE);// Create a window for display.
 
     Mat traj = Mat::zeros(600, 600, CV_8UC3);
-
     for (int numFrame = 2; numFrame < MAX_FRAME; numFrame++) {
-        string filename =
-                root_path +
-                (boost::format("/data_odometry_color/dataset/sequences/00/image_2/%06d.png") % numFrame).str();
-        //cout << numFrame << endl;
+        string filename = root_path;
+        filename += (boost::format("/data_odometry_color/dataset/sequences/00/image_2/%06d.png") % numFrame).str();
+        cout << numFrame << endl;
+
         const Mat currImage_c = imread(filename);
         cvtColor(currImage_c, currImage, COLOR_BGR2GRAY);
 
@@ -131,38 +149,24 @@ int main(int argc, char **argv) {
         // 5-point algorithm
         recoverPose(E, currFeatures, prevFeatures, R, t, focal, pp, mask);
 
-        Mat prevPts(2, int(prevFeatures.size()), CV_64F);
-        Mat currPts(2, int(currFeatures.size()), CV_64F);
+        const double scale = getAbsoluteScale(numFrame);
 
-        //this (x,y) combination makes sense as observed from the source code of triangulatePoints on GitHub
-        for (int i = 0; i < prevFeatures.size(); i++) {
-            prevPts.at<double>(0, i) = prevFeatures.at(i).x;
-            prevPts.at<double>(1, i) = prevFeatures.at(i).y;
-
-            currPts.at<double>(0, i) = currFeatures.at(i).x;
-            currPts.at<double>(1, i) = currFeatures.at(i).y;
-        }
-
-        scale = getAbsoluteScale(numFrame);
-
-        //cout << "Scale is " << scale << endl;
+        cout << "Scale is " << scale << endl;
 
         if ((scale > 0.1) && (t.at<double>(2) > t.at<double>(0)) && (t.at<double>(2) > t.at<double>(1))) {
-
             t_f = t_f + scale * (R_f * t);
             R_f = R * R_f;
-
         } else {
-            //cout << "scale below 0.1, or incorrect translation" << endl;
+            cout << "scale below 0.1, or incorrect translation" << endl;
         }
 
         // lines for printing results
-        // myfile << t_f.at<double>(0) << " " << t_f.at<double>(1) << " " << t_f.at<double>(2) << endl;
+        myfile << t_f.at<double>(0) << " " << t_f.at<double>(1) << " " << t_f.at<double>(2) << endl;
 
         // a redetection is triggered in case the number of feautres being trakced go below a particular threshold
         if (prevFeatures.size() < MIN_NUM_FEAT) {
-            //cout << "Number of tracked features reduced to " << prevFeatures.size() << endl;
-            //cout << "trigerring redection" << endl;
+            cout << "Number of tracked features reduced to " << prevFeatures.size() << endl;
+            cout << "trigerring redection" << endl;
             featureDetection(prevImage, prevFeatures);
             featureTracking(prevImage, currImage, prevFeatures, currFeatures, status);
 
@@ -171,30 +175,9 @@ int main(int argc, char **argv) {
         prevImage = currImage.clone();
         prevFeatures = currFeatures;
 
-        {
-            const int x = int(t_f.at<double>(0)) + 300;
-            const int y = int(t_f.at<double>(2)) + 100;
-            circle(traj, Point(x, y), 1, CV_RGB(255, 0, 0), 2);
-        }
-
-        rectangle(traj, Point(10, 30), Point(550, 50), CV_RGB(0, 0, 0), FILLED);
-
-        {
-//            char text[100];
-//            sprintf(text, "Coordinates: x = %02fm y = %02fm z = %02fm", t_f.at<double>(0), t_f.at<double>(1),
-//                    t_f.at<double>(2));
-            string text = (boost::format("Coordinates: x = %02fm y = %02fm z = %02fm")
-                           % t_f.at<double>(0) % t_f.at<double>(1) % t_f.at<double>(2)).str();
-
-            const int fontFace = FONT_HERSHEY_PLAIN;
-            const double fontScale = 1;
-            const int thickness = 1;
-            const cv::Point textOrg(10, 50);
-            putText(traj, text, textOrg, fontFace, fontScale, Scalar::all(255), thickness, 8);
-        }
-
-        imshow("Road facing camera", currImage_c);
+        write_trajectory(t_f, traj);
         imshow("Trajectory", traj);
+        imshow("Road facing camera", currImage_c);
 
         waitKey(1);
 
@@ -204,8 +187,8 @@ int main(int argc, char **argv) {
     double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
     cout << "Total time taken: " << elapsed_secs << "s" << endl;
 
-    //cout << R_f << endl;
-    //cout << t_f << endl;
+    cout << R_f << endl;
+    cout << t_f << endl;
 
     return 0;
 }
