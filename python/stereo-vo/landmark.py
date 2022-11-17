ba807b1d375159a2f6d23f63f9bacf0b904c8e9c
applejxd <9639592+applejxd@users.noreply.github.com>
2022-11-18 07:49:13 +0900

    update

---
+++ python/stereo-vo/landmark.py
@@ -0,0 +1,9 @@
+landmark_idx = 0
+
+
+class Landmark:
+    def __init__(self, current_pos, point_pos):
+        global landmark_idx
+        self.id = landmark_idx
+        landmark_idx += 1
+        self.abs_pos
