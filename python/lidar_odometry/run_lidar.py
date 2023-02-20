140d6d140080b5eaa936c6cb0328e63fbcc3f40a
applejxd <9639592+applejxd@users.noreply.github.com>
2023-02-21 07:49:32 +0900

    update

--- python/lidar_odometry/run_lidar.py
+++ python/lidar_odometry/run_lidar.py
@@ -16,6 +16,13 @@ def read_pcd(lidar_file: str) -> o3d.geometry.PointCloud:
     return pcd
 
 
+def update_pcd(lidar_file: str, pcd: o3d.geometry.PointCloud) -> None:
+    lidar_data = np.fromfile(lidar_file, dtype=np.float32).reshape(-1, 4)
+
+    pcd.points = o3d.utility.Vector3dVector(lidar_data[:, :3])
+    pcd.colors = o3d.utility.Vector3dVector(np.tile(lidar_data[:, 3], (3, 1)).T)
+
+
 def main():
     lidar_files = glob.glob(os.path.join(
         config["root_path"], config["sequence_num"], "velodyne", "*.bin"))
@@ -25,10 +32,11 @@ def main():
     pcd = read_pcd(lidar_files[0])
     vis.add_geometry(pcd)
 
+    frame = o3d.geometry.TriangleMesh.create_coordinate_frame()
+    vis.add_geometry(frame)
+
     for lidar_file in lidar_files[1:]:
-        tmp_pcd = read_pcd(lidar_file)
-        pcd.points = tmp_pcd.points
-        pcd.colors = tmp_pcd.colors
+        update_pcd(lidar_file, pcd)
 
         vis.update_geometry(pcd)
         vis.poll_events()
