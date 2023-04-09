45d42852e90c5411e59f6f3d0107b35f6f56ba4d
applejxd <9639592+applejxd@users.noreply.github.com>
2023-04-09 20:41:36 +0900

    update data conversion examples

--- cpp/samples/eigen_sample.cpp
+++ cpp/samples/eigen_sample.cpp
@@ -5,15 +5,69 @@
 #include <Eigen/Dense>
 #include <iostream>
 
-using namespace std;
-using namespace Eigen;
+namespace Eigen {
+// 行優先メモリ確保行列
+template <typename T>
+using RMatrix =
+    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
+using RMatrixXd = RMatrix<double>;
+}  // namespace Eigen
 
 int main() {
-  Matrix2f A;
-  A << 1, 2, 2, 3;
+  Eigen::MatrixXd A(2, 2);
+  {
+    A << 1, 2, 3, 4;
+    std::cout << "A (Case 1):" << std::endl << A << std::endl << std::endl;
+  }
 
-  SelfAdjointEigenSolver<Matrix2f> es(A);
-  if (es.info() != Success) abort();
-  cout << "固有値：\n" << es.eigenvalues() << endl;
-  cout << "固有ベクトル：\n" << es.eigenvectors() << endl;
+  // Eigen::MatrixXd -> std::vector<double>
+  {
+    std::vector<double> vector(4);
+    // 行優先で格納
+    Eigen::Map<Eigen::RMatrixXd>(vector.data(), 2, 2) = A;
+
+    std::cout << "transferred vector (Case 2):" << std::endl;
+    for (uint i = 0; i < 4; ++i) std::cout << vector[i] << ",";
+    std::cout << std::endl << std::endl;
+  }
+
+  // std::vector<double> -> Eigen::MatrixXd
+  {
+    std::vector<double> vector = {4, 3, 2, 1};
+    // 行優先で格納
+    A = Eigen::Map<Eigen::RMatrixXd>(vector.data(), 2, 2);
+    std::cout << "A (Case 3):" << std::endl << A << std::endl << std::endl;
+  }
+
+  // Eigen::MatrixXd -> double**
+  {
+    A << 1, 2, 3, 4;
+    double vector[2][2];
+    Eigen::Map<Eigen::RMatrixXd>(&(vector[0][0]), 2, 2) = A;
+
+    std::cout << "transferred vector (Case 4):" << std::endl;
+    for (uint i = 0; i < 2; ++i)
+      for (uint j = 0; j < 2; ++j) std::cout << vector[i][j] << ",";
+    std::cout << std::endl << std::endl;
+  }
+
+  // std::vector<std::vector<<double>> -> Eigen::MatrixXd
+  {
+    std::vector<std::vector<double>> vector = {{1, 2}, {2, 3}};
+    for (uint i = 0; i < 2; ++i)
+      A.row(i) = Eigen::Map<Eigen::VectorXd>(vector[i].data(), 2);
+    std::cout << "A (Case 5):" << std::endl << A << std::endl << std::endl;
+  }
+
+  // 対角化
+  {
+    Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> es(A);
+    if (es.info() != Eigen::Success) abort();
+    std::cout << "eigenvalues:" << std::endl
+              << es.eigenvalues() << std::endl
+              << std::endl;
+    std::cout << "eigenvectors:" << std::endl
+              << es.eigenvectors() << std::endl
+              << std::endl;
+  }
 }
